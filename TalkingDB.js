d0113e66e2e85cd776912c47edcc5e4e80bff6a8
Kishore Annam <annamkishore@gmail.com>
2018-01-27 01:29:53 +0530

    accessing mongodb

---
+++ TalkingDB.js
@@ -0,0 +1,47 @@
+
+var MongoClient = require('mongodb').MongoClient;
+
+class TalkingDB {
+    constructor() {
+        this.url = "mongodb://localhost:27017/";
+        this.dbName = "talkingdb";
+        this.collectionName = "textcollection";
+    }
+
+    putText(textObj) {
+        MongoClient.connect(this.url).then((db) => {
+            let dbo = db.db(this.dbName);
+            this.myConnection = dbo.collection(this.collectionName);
+            let query = {userId: textObj.userId};
+            this.myConnection.findOneAndUpdate(query, {$set: textObj}, {upsert: true}).then(data => {
+                db.close();
+            });
+        });
+    }
+
+    getText(userObjQuery) {
+        let dbObj = null;
+        return MongoClient.connect(this.url).then(db => {
+            var dbo = db.db(this.dbName);
+            let query = {userId: userObjQuery.userId};
+            dbObj = db;
+            return dbo.collection(this.collectionName).findOne(query)
+        }).then((data) => {
+            dbObj.close();
+            return data;
+        });
+    }
+}
+
+exports.TalkingDB = TalkingDB;
+
+function testRun() {
+    let db = new TalkingDB();
+    // db.putText({userId:"11", userText: "hi 11"});
+
+    db.getText({userId: "11"}).then(data => {
+        console.log(data);
+    });
+}
+
+testRun();
--- index.js
+++ index.js
@@ -3,4 +3,30 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("src"));
+
+let {TalkingDB} = require("./TalkingDB");
+let db = new TalkingDB();
+
+// save user text
+// http://localhost:9090/putText?userId=11&userText=hello
+app.get("/putText", function (req, res) {
+    console.log("put");
+    let userTextObj = {userId: req.query.userId, userText: req.query.userText};
+    console.log(userTextObj);
+    db.putText(userTextObj);
+    res.end();
+});
+
+// get user text
+// http://localhost:9090/putText?userId=11
+app.get("/getText", function (req, res) {
+    console.log("get");
+    let userObj = {userId: req.query.userId};
+    console.log(db.getText(userObj));
+    res.json();
+});
+
 app.listen(9090);
+
+// db.putText();
+// db.getText();
